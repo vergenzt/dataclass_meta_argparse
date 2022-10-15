4ee5c19763a8c4ba0b939ee6537c1d21e010186f
Tim Vergenz <vergenzt@gmail.com>
2022-10-14 23:36:09 -0400

    wip

--- dataclass_meta_argparse.py
+++ dataclass_meta_argparse.py
@@ -1,144 +1,220 @@
-from functools import partial, partialmethod
+r'''
+Tools to help build ArgumentParsers out of dataclass metadata.
+
+To use:
+
+ 1. Create a dataclass whose fields you'd like to populate from `argparse` arguments,
+    set its metaclass to `DataclassMetaArgumentParser`, and pass any additional `ArgumentParser`
+    keyword args to the metaclass.
+
+ 2. Associate command line arguments with dataclass fields by adding lists of `argparse_arguments`
+    to fields' `metadata` dicts under the `ARGS` key. (An `argparse_argument` is essentially a
+    `functools.partial` on `ArgumentParser.add_argument`, but where the actual parser instance
+    is added last.)
+
+ 3. Apply any additional customizations to the ArgumentParser after the class is declared through
+    the class's `argument_parser` attribute.
+
+ 4. Get an instance of your dataclass from parsed command line arguments via the class's
+    new `from_args` method.
+
+Example:
+
+>>> @argument_parser_from_dataclass_meta(prog='my-cli')
+... @dataclass
+... class MyCliArgs:
+...   'My CLI app that does a thing'
+...   my_arg: int = field(metadata={
+...     ARGS: [argparse_argument(
+...       '--myarg', '-m',
+...       metavar='INT',
+...       type=int,
+...       default=5,
+...       help='my integer argument',
+...     )],
+...   })
+...   remainder: List[str] = field(metadata={
+...     ARGS: [argparse_argument(
+...       nargs=argparse.ZERO_OR_MORE,
+...       metavar='STR',
+...       help='remaining string arguments',
+...     )],
+...   })
+>>> MyCliArgs.from_args(['--myarg', '7', 'foo', 'bar', 'baz'])
+MyCliArgs(my_arg=7, remainder=['foo', 'bar', 'baz'])
+>>> MyCliArgs.argument_parser.print_help()
+usage: my-cli [-h] [--myarg INT] [STR ...]
+<BLANKLINE>
+positional arguments:
+  STR                  remaining string arguments
+<BLANKLINE>
+options:
+  -h, --help           show this help message and exit
+  --myarg INT, -m INT  my integer argument
+'''
+
+import argparse
 import os
 import re
 import sys
-from abc import ABC, abstractmethod
-from argparse import Action, ArgumentParser, HelpFormatter
-from dataclasses import dataclass, field, fields
+from argparse import Action, ArgumentParser, Namespace
+from dataclasses import asdict, dataclass, field, fields
+from functools import partial, partialmethod, wraps
 from logging import debug
-from typing import Callable, ClassVar, Dict, Iterator, List, Tuple, Type, Union
-
-from trycast import isassignable
-
-
-ARGSPEC_KEY: object = object()
-
-
-class ArgSpec(partial):
-  '''
-  An ArgSpec is a saved set of arguments to `ArgumentParser.add_argument`, for later
-  application to a specific ArgumentParser instance.
-
-  Instantiate one by calling `ArgSpec(...)` with the same arguments you would use
-  on `ArgumentParser.add_argument`.
-  
-  Exception: You should not specify a `dest`; this will be supplied to `add_argument` as
-  a `kwarg` with the name of the dataclass field the argument is assigned to.
-  '''
-  func = partial(partialmethod, ArgumentParser.add_argument)
+from typing import (
+    Any,
+    Callable,
+    ClassVar,
+    Concatenate,
+    Dict,
+    Generic,
+    List,
+    Mapping,
+    Optional,
+    ParamSpec,
+    Type,
+    TypeAlias,
+    TypeVar,
+    Union
+)
+from wsgiref import validate
 
 
-class _DataclassArgumentParserMeta(type['DataclassArgumentParser']):
-  def __new__(cls, clsname, bases, dct, **parser_init_kws):
-    self = super().__new__(cls, clsname, bases, dct)
-    self._argumentparser_kwargs = parser_init_kws
+Self = TypeVar('Self', bound=ArgumentParser)
+P = ParamSpec('P')
+R = TypeVar('R', ArgumentParser, Action)
 
 
-@dataclass
-class DataclassArgumentParser(metaclass=_DataclassArgumentParserMeta):
-  """
-  Base class to help build ArgumentParsers out of dataclass metadata.
+def _partial_instance_method(source_fn: Callable[Concatenate[Self, P], R]) -> Callable[P, Callable[[Self], R]]:
+    def outer(*outer_args: P.args, **outer_kwargs: P.kwargs) -> Callable[[Self], R]:
+        def inner(self: Self, **kwargs) -> R:
+            return source_fn(self, *outer_args, **outer_kwargs, **kwargs)
+        return inner
+    return outer
 
-  To use:
 
-   1. Create a dataclass that inherits this class whose fields you'd like to populate
-      from `argparse.ArgumentParser` arguments. Declare those fields using
-      `dataclasses.field`. 
+argparse_argument: Callable[..., Callable[[ArgumentParser], Action]]
+argparse_argument = _partial_instance_method(ArgumentParser.add_argument)
+argparse_argument.__doc__ = '''
+    An `argparse_argument` is a saved set of arguments to `ArgumentParser.add_argument`, for
+    later application to a specific `ArgumentParser` instance.
 
-   2. Configure command line arguments to populate a dataclass field by setting
-      `ARGSPEC_KEY` on its `metadata` dict to a list of `ArgSpec`s -- which are essentially
-      `functools.partial`'s on `ArgumentParser.add_argument`.
+    Instantiate one by calling `argparse_argument(...)` with the same arguments you would
+    give to the `add_argument` method of an `ArgumentParser` instance.
 
-      Note: You should not use the `dest` keyword arg in your `ArgSpec`s, as this class
-      specifies the name of the dataclass field as the argument destination via an
-      additional `dest` kwarg.
+    Exception: You should not specify a `dest`; this will be supplied to `add_argument`
+    later with the name of the dataclass field whose metadata the `argparse_argument` is a
+    member of.
 
-  Example:
-
-    >>> @dataclass
-    ... class MyCliArgs(metaclass=CliArgsBase, prog='my_cli.py'):
-    ...   'My CLI app that does a thing'
-    ...
-    ...   my_arg: int = field(metadata={
-    ...     ARGSPEC_KEY: [ArgSpec(
-    ...       '--myarg', '-m',
-    ...       metavar='INT',
-    ...       type=int,
-    ...       default=5,
-    ...       help='my integer argument',
-    ...     )],
-    ...   })
-    ...
-    ...   remainder: List[str] = field(metadata={
-    ...     ARGSPEC_KEY: [ArgSpec(
-    ...       nargs=argparse.ZERO_OR_MORE,
-    ...       metavar='STR',
-    ...       help='remaining string arguments',
-    ...     )],
-    ...   })
-    
-    >>> MyCliArgs.parse('foo', 'bar', '--myarg', '7', 'baz')
-    MyCliArgs(my_arg=7, remainder=['foo', 'bar', 'baz'])
-
-    >>> MyCliArgs._get_argument_parser().format_help()
-    'usage: myprogram.py [-h] [--foo FOO]
-
+    >>> my_arg_fn = argparse_argument('-f', '--foo', action='store_true', help='bar')
+    >>> parser = ArgumentParser(prog='baz')
+    >>> action = my_arg_fn(parser)
+    >>> parser.print_help()
+    usage: baz [-h] [-f]
+    <BLANKLINE>
     options:
-    -h, --help  show this help message and exit
-    --foo FOO   foo help
-  """
+      -h, --help  show this help message and exit
+      -f, --foo   bar
+'''
 
-  _argumentparser_kwargs: ClassVar[Dict] = field(default_factory=dict, kw_only=True)
 
-  @classmethod
-  def parser(cls) -> ArgumentParser:
-    parser = ArgumentParser(description=cls.__doc__, **cls._argumentparser_kwargs)
-    [
-      add_arg_fn(parser, dest=field.name)
-      for field in fields(cls) 
-      for add_arg_fn in field.metadata[ARGSPEC_KEY]
-    ]
-    return parser
+ARGS = object()
 
-  @staticmethod
-  def _envize_string(s: str, keep_case: Union[bool, Callable[[str],bool]] = False) -> str:
+
+def default_envize_string(s: str, keep_case: Union[bool, Callable[[str], bool]] = lambda s: len(s) == 1) -> str:
     subbed = re.sub(r'[^a-zA-Z0-9]+', '_', s).strip('_')
     return subbed if (keep_case is True or callable(keep_case) and keep_case(subbed)) else subbed.upper()
 
-  @classmethod
-  def _arguments_from_env(cls, parser: ArgumentParser) -> Iterator[str]:
 
-    envized_opts: Dict[str, str] = {
-      cls._envize_string(opt, keep_case=lambda s: len(s) == 1): opt
-      for opt in parser._option_string_actions.keys()
-    }
+ArgumentParserT = TypeVar('ArgumentParserT', bound=ArgumentParser)
+ArgumentParserP = ParamSpec('ArgumentParserP')
+T = TypeVar('T')
 
-    for envvar, envvar_val in sorted(os.environ.items()):
-      if not envvar.startswith(cls._ENVVAR_PREFIX) or not envvar_val:
-        continue
+def _param_spec_scope_container(ArgumentParser: Callable[ArgumentParserP, ArgumentParserT]):
 
-      if not (match := re.match(cls._ENVVAR_RE, envvar)):
-        raise ValueError(f'Envvar {envvar}: Could not parse. Should match {repr(cls._ENVVAR_RE)}.')
+    def argument_parser_from_dataclass_meta(
+        include_env: bool = True,
+        env_prefix: Optional[str] = None,
+        envize_str_fn: Callable[[str], str] = default_envize_string,
+        validate_field_types: bool = False,
+        *args: ArgumentParserP.args,
+        **kwargs: ArgumentParserP.kwargs,
+    ):
+        class _argument_parser_from_dataclass_meta_base:
+            argument_parser: ArgumentParserT = ArgumentParser(*args, **kwargs)
 
-      if (_envized_opt := match['envized_opt']) not in envized_opts:
-        raise ValueError(f'Envvar {envvar}: Unrecognized option {repr(_envized_opt)}. Must be one of {envized_opts.keys()}.')
+            def __init_subclass__(cls):
 
-      opt = envized_opts[_envized_opt]
-      action = parser._option_string_actions[opt]
+                for field in fields(cls):
+                    for add_arg_fn in field.metadata[ARGS]:
+                        add_arg_fn(cls.argument_parser, dest=field.name)
 
-      extra_args = [opt] + ([envvar_val] if action.nargs is None or action.nargs else [])
-      debug(f'Extra args from env var {envvar}: {extra_args}')
-      yield from extra_args
+                if validate_field_types:
+                    old_post_init = getattr(cls, '__post_init__', None)
 
-  @classmethod
-  def parse(cls: Type['DataclassArgumentParser'], argv: List[str] = sys.argv[1:]) -> Tuple['DataclassArgumentParser', ArgumentParser]:
-    parser = cls.parser()
-    cmd = cls._get_command(argv)
-    argv = list(cls._arguments_from_env(parser, cmd)) + argv
-    args_namespace = parser.parse_args(argv)
-    args = cls(**args_namespace.__dict__)
-    return args, parser
+                    def new_post_init(self, *args, **kwargs):
+                        self._validate_types(self)
+                        if old_post_init:
+                            old_post_init(self, *args, **kwargs)
 
-  def __post_init__(self):
-    for field in fields(self):
-      assert isassignable(val := getattr(self, field.name), field.type), f'{field.name} {val} should be a {field.type}!'
+                    setattr(cls, '__post_init__', new_post_init)
+
+            @classmethod
+            def _validate_types(cls, self):
+                from trycast import isassignable
+                for field in fields(self):
+                    field_val = getattr(self, field.name)
+                    assert isassignable(field_val, field.type), f'{field.name} {field_val} should be a {field.type}!'
+
+            @classmethod
+            def _extra_args_from_env(cls, env: Mapping[str, str] = os.environ) -> List[str]:
+                parser = cls.argument_parser
+                prefix: str = env_prefix or envize_str_fn(parser.prog)
+
+                envized_opts: Dict[str, str] = {
+                    envize_str_fn(opt): opt
+                    for opt in parser._option_string_actions.keys()
+                }
+
+                extra_args: List[str] = []
+                for envvar, envvar_val in env.items():
+                    if not envvar.startswith(prefix + '_') or not envvar_val:
+                        continue
+
+                    envized_opt = envvar[len(prefix)+1:]
+                    if envized_opt not in envized_opts:
+                        raise ValueError(
+                            f'Envvar {envvar}: Unrecognized option {repr(envized_opt)}. Must be one of {set(envized_opts.keys())}.')
+
+                    opt = envized_opts[envized_opt]
+                    action = parser._option_string_actions[opt]
+
+                    extra_arg = [
+                        opt] + ([envvar_val] if action.nargs is None or action.nargs else [])
+                    extra_args = extra_arg + extra_args  # prepend
+
+                    debug(f'Extra arg from env var {envvar}: {extra_arg}')
+
+                return extra_args
+
+            @classmethod
+            def from_args(cls, argv: Optional[List[str]] = None) -> '_argument_parser_from_dataclass_meta_base':
+                if argv is None:
+                    argv = (cls._extra_args_from_env() if include_env else []) + sys.argv[1:]
+
+                parser = cls.argument_parser
+                args_ns: Namespace = parser.parse_args(argv)
+                args = cls(**args_ns.__dict__)
+                return args
+
+        def wrapper_generator(cls: Type[T]) -> Type[T]:
+            @wraps(cls, updated=[])
+            class wrapper(cls, _argument_parser_from_dataclass_meta_base): # type:ignore
+                pass
+            return wrapper
+
+        return wrapper_generator
+
+    return argument_parser_from_dataclass_meta
+
+argument_parser_from_dataclass_meta = _param_spec_scope_container(ArgumentParser)
